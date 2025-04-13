5c1024a4bc4d4383518be734eb8346c64e664314
Nico <akebra77@gmail.com>
2025-04-13 20:00:03 +0200

    created receipt-service project

---
+++ receipt-service/src/main/java/com/pengeboks/receipt/ReceiptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pengeboks.receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptServiceApplication.class, args);
+	}
+
+}
